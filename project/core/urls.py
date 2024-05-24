2fc94027946cfb7e729c12789f5e65616e3aef11
Pablo Martinez <palunmartinez@gmail.com>
2024-05-24 18:07:21 -0300

    feat: Galeria_fotos. Retoques en css

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, sobre_mi
+from .views import index, sobre_mi, galeria_fotos
 
 app_name = 'core'
 
 urlpatterns = [
     path('', index, name='index'),
     path('core/sobre_mi', sobre_mi, name='sobre_mi'),
+    path('core/galeria_fotos', galeria_fotos, name='galeria_fotos'),
 ]
--- project/core/views.py
+++ project/core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from perfil.models import Mascota
 
 
 def index(request):
@@ -6,3 +7,10 @@ def index(request):
 
 def sobre_mi(request):
     return render(request, 'core/sobre_mi.html')
+
+def galeria_fotos(request):
+    mascotas = Mascota.objects.all()
+    context = {
+        'mascotas': mascotas,
+    }
+    return render(request, 'core/galeria_fotos.html', context)
